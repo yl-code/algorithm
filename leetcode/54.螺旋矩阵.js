783cf0ea97a1db0b492fc0a15f7385144a407094
杨磊 <yl.code@qq.com>
2021-12-02 09:19:33 +0800

    2021-12-2

---
+++ leetcode/54.螺旋矩阵.js
@@ -0,0 +1,76 @@
+/*
+ * @lc app=leetcode.cn id=54 lang=javascript
+ *
+ * [54] 螺旋矩阵
+ */
+
+// @lc code=start
+/**
+ * @param {number[][]} matrix
+ * @return {number[]}
+ */
+var spiralOrder = function (matrix) {
+  let top = 0,
+    right = matrix[0].length - 1,
+    bottom = matrix.length - 1,
+    left = 0,
+    res = [];
+
+  while (top <= bottom && left <= right) {
+    for (let i = left; i <= right; i++) res.push(matrix[top][i]);
+    top++;
+
+    for (let i = top; i <= bottom; i++) res.push(matrix[i][right]);
+    right--;
+
+    if (top > bottom || left > right) break;
+
+    for (let i = right; i >= left; i--) res.push(matrix[bottom][i]);
+    bottom--;
+
+    for (let i = bottom; i >= top; i--) res.push(matrix[i][left]);
+    left++;
+  }
+
+  return res;
+};
+
+var spiralOrder_2 = function (matrix) {
+  const maxRow = matrix.length;
+  const maxColumn = matrix[0].length;
+  const res = [];
+
+  for (let count = 0, x = -1, y = 0, loop = 0, allCount = maxRow * maxColumn; count < allCount; loop++) {
+    // 👉
+    while (count <= allCount && x + 1 <= maxColumn - 1 - loop) {
+      count++;
+      x++;
+      res.push(matrix[x][y]);
+    }
+
+    // 👇
+    while (count < allCount && y + 1 <= maxRow - 1 - loop) {
+      count++;
+      y++;
+      res.push(matrix[x][y]);
+    }
+
+    // 👈
+    while (count < allCount && x - 1 >= loop) {
+      count++;
+      x--;
+      res.push(matrix[x][y]);
+    }
+
+    // 👆
+    while (count < allCount && y - 1 > loop) {
+      count++;
+      y--;
+      res.push(matrix[x][y]);
+    }
+  }
+
+  console.log(res);
+  return res;
+};
+// @lc code=end
