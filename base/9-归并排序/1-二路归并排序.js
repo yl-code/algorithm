4b34622be28e6eea0a9959ab430e1791d529eb8f
yl <yl.code@qq.com>
2022-08-08 15:28:44 +0800

    二路归并

--- base/9-归并排序/1-二路归并排序.js
+++ base/9-归并排序/1-二路归并排序.js
@@ -5,6 +5,7 @@
  * @param {*} r 待排序数组区间的右边界
  */
 const merge_sort = (arr, l, r) => {
+  // 只剩下一个元素 或者 区间为空
   if (l >= r) return;
 
   // 计算待排序数组的中间索引
@@ -17,7 +18,7 @@ const merge_sort = (arr, l, r) => {
   // 初始化一个临时数组
   const temp = [];
 
-  // 初始化两个指针，分别指向两个区间的第一个元素
+  // 初始化两个指针，分别指向左右两个区间的第一个元素
   let p1 = l;
   let p2 = mid + 1;
 
@@ -26,7 +27,7 @@ const merge_sort = (arr, l, r) => {
     // 当 p2 走过了 右区间 的 右边界
     // 或者
     // p1 没走过 左区间 的右边界 并且 p1 指针的元素 小于 p2 指针的元素
-    // 直接将 p1 指针指向的元素添加到 临时数组中
+    // 则，直接将 p1 指针指向的元素添加到 临时数组中
     if (p2 > r || (p1 <= mid && arr[p1] < arr[p2])) {
       temp.push(arr[p1++]);
     } else {
@@ -42,6 +43,30 @@ const merge_sort = (arr, l, r) => {
   }
 };
 
+const mergeSort = (arr, l, r) => {
+  if (l >= r) return;
+
+  const mid = (l + r) >> 1;
+
+  mergeSort(arr, l, mid);
+  mergeSort(arr, mid + 1, r);
+
+  const temp = [];
+  let p1 = l;
+  let p2 = mid + 1;
+  while (p1 <= mid || p2 <= r) {
+    if (p2 > r || (p1 <= mid && arr[p1] < arr[p2])) {
+      temp.push(arr[p1++]);
+    } else {
+      temp.push(arr[p2++]);
+    }
+  }
+
+  for (let i = l; i <= r; i++) {
+    arr[i] = temp[i - l];
+  }
+};
+
 const arr = [2, 5, 1, 7, 3, 9, 4, 6, 8];
-merge_sort(arr, 0, arr.length - 1);
+mergeSort(arr, 0, arr.length - 1);
 console.log(arr);
