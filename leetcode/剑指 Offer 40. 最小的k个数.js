5a75f866ee51b79eacaca642fe19693ac18299d3
yl <yl.code@qq.com>
2022-12-20 11:12:34 +0800

    2021-12-12

--- leetcode/剑指 Offer 40. 最小的k个数.js
+++ leetcode/剑指 Offer 40. 最小的k个数.js
@@ -1,7 +1,9 @@
 /**
- *  https://leetcode-cn.com/problems/zui-xiao-de-kge-shu-lcof/
- *  输入整数数组 arr ，找出其中最小的 k 个数。
- *  例如，输入4、5、1、6、2、7、3、8这8个数字，则最小的4个数字是1、2、3、4。
+ * 剑指 Offer 40. 最小的k个数
+ * https://leetcode-cn.com/problems/zui-xiao-de-kge-shu-lcof/
+ *
+ * 输入整数数组 arr ，找出其中最小的 k 个数。
+ * 例如，输入4、5、1、6、2、7、3、8这8个数字，则最小的4个数字是1、2、3、4。
  */
 
 class Heap {
@@ -17,7 +19,8 @@ class Heap {
     this.compare = compare;
   }
 
-  swap = (i, j) => ([this.data[i], this.data[j]] = [this.data[j], this.data[i]]);
+  swap = (i, j) =>
+    ([this.data[i], this.data[j]] = [this.data[j], this.data[i]]);
 
   size = () => this.data.length;
 
